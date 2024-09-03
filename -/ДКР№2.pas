519eea01f076fc77ff95ba1813c0ec2e4906c233
JustEternity <143828888+JustEternity@users.noreply.github.com>
2024-09-03 13:58:10 +0300

    add - rep

---
+++ -/ДКР№2.pas
@@ -0,0 +1,72 @@
+﻿program CheckSquareIndices;
+var
+  arr: array of Integer;
+  i, Size: Integer;
+  allSquareIndixes, Result: Boolean;
+  mainString, subString: String;
+
+function AreAllElementsSquareOfIndixes(Size: Integer; arrayToCheck: array of Integer): Boolean;
+var
+  indexSquared: Integer;
+  k:Integer;
+  i:Integer;
+begin
+  AreAllElementsSquareOfIndixes := true;
+  k := Size;
+  for i := 0 to k - 1 do
+  begin
+    indexSquared := (i + 1)* (i + 1);
+    if arrayToCheck[i] <> indexSquared then
+    begin
+      AreAllElementsSquareOfIndixes := false;
+      Break;
+    end;
+  end;
+end;
+
+function IsSubsequence(mainString, subString: String): Boolean;
+var
+  m, j: Integer;
+begin
+  m := 1;
+  j := 1;
+
+  while (m <= Length(mainString)) and (j <= Length(subString)) do
+  begin
+    if mainString[m] = subString[j] then
+      j := j + 1;
+    m := m + 1;
+  end;
+
+  Result := (j > Length(subString));
+end;
+
+begin
+  Readln(Size);
+  SetLength(arr, Size);
+  for i := 0 to Size - 1 do
+  begin
+    Readln(arr[i]);
+    Write(arr[i], ' ');
+  end;
+  Writeln;
+  
+  allSquareIndixes := AreAllElementsSquareOfIndixes(Size, arr);
+  if allSquareIndixes then
+    Writeln('Массив параболический')
+  else
+    Writeln('Массив не параболический');
+  
+  Write('Введите исходную строку: ');
+  Readln(mainString);
+  
+  Write('Введите подстроку для проверки: ');
+  Readln(subString);
+
+  Result := IsSubsequence(mainString, subString);
+
+  if Result then
+    Writeln(subString, ' является подпоследовательностью ', mainString)
+  else
+    Writeln(subString, ' не является подпоследовательностью ', mainString);
+end.
---
+++ -/ДКР№3(2).pas
@@ -0,0 +1,68 @@
+﻿program KP3;
+uses Crt;
+
+function F(x: Real): Real;
+begin
+  F := 2*x*x*x - x*x + x + 11;
+end;
+
+function MidpointRectanglesIntegration(a, b: Real; n: Integer): Real;
+var
+  h, x, sum: Real;
+  i: Integer;
+begin
+  h := (b - a) / n;
+  sum := 0;
+  for i := 0 to n - 1 do
+  begin
+    x := a + h * (i + 0.5);
+    sum := sum + F(x);
+  end;
+  MidpointRectanglesIntegration := h * sum;
+end;
+
+var
+  a, b, S: Real;
+  n: Integer;
+  res: Char;
+  option: Integer;
+
+begin
+  repeat
+    Writeln('Выберите действие:');
+    Writeln('1. Рассчитать площадь фигуры');
+    Writeln('2. Выйти из программы');
+    Readln(option);
+    ClrScr;
+
+    case option of
+      1:
+      begin
+        Writeln('Введите пределы интегрирования a и b через пробел:');
+        Readln(a, b);
+        Writeln('Введите количество отрезков для деления:');
+        Readln(n);
+        ClrScr;
+        S := MidpointRectanglesIntegration(a, b, n);
+        Writeln('Площадь фигуры, ограниченной кривой, равна: ', S:0:10);
+
+        Writeln('Хотите оценить погрешность полученного результата? (y/n)');
+        Readln(res);
+        if (res = 'y') or (res = 'Y') then
+        begin
+          if n = 1 then
+          begin
+            Writeln('Нельзя оценить погрешность, если используется 1 отрезок.');
+          end
+          else
+          begin
+            S := MidpointRectanglesIntegration(a, b, n);
+            Writeln('Площадь фигуры равна: ', S:0:10);
+            Writeln('Вычисленная погрешность: ', Abs(S - MidpointRectanglesIntegration(a, b, n*2)));
+          end;
+        end;
+      end;
+      2: Writeln('До свидания!');
+    end;
+  until option = 2;
+end.
