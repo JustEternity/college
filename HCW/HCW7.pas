e42c2f953ee0618a4a3e1f30249fefdb615d78a9
JustEternity <143828888+JustEternity@users.noreply.github.com>
2024-09-03 13:56:31 +0300

    Add HCW rep

---
+++ HCW/HCW7.pas
@@ -0,0 +1,40 @@
+﻿program HCW7;
+
+uses peano_curve, GraphABC;
+
+var
+  startX, startY: Integer;
+  scale: Integer;
+  depth: Integer;
+
+procedure KeyDown(Key: Integer);
+begin
+  case Key of
+    VK_Left: startX := startX - 10;
+    VK_Right: startX := startX + 10;
+    VK_Up: startY := startY - 10;
+    VK_Down: startY := startY + 10;
+    189: if scale > 1 then scale := scale - 1;
+    187: scale := scale + 1;
+    VK_W: if depth < 10 then depth := depth + 1;
+    VK_S: if depth > 1 then depth := depth - 1;
+  end;
+LockDrawing;
+ClearWindow(clWhite);
+init(depth, startX, startY, scale);
+Redraw;
+end;
+
+begin
+SetWindowSize(600, 600);
+ClearWindow(clWhite);
+SetPenColor(clBlack);
+OnKeyDown := KeyDown;
+startX := WindowWidth div 2 - 125;
+startY := WindowHeight div 2 + 125;
+scale := 10;
+depth := 3;
+LockDrawing;
+init(depth, startX, startY, scale);
+Redraw;
+end.
---
+++ HCW/peano_curve.pas
@@ -0,0 +1,59 @@
+﻿unit peano_curve;
+
+uses GraphABC;
+
+var
+startX, startY: Integer;
+currentX, currentY: Integer;
+currentAngle: Real;
+scale: Integer;
+depth: Integer;
+
+procedure turnRight(angle: Real);
+begin
+  currentAngle := currentAngle - angle;
+end;
+
+procedure turnLeft(angle: Real);
+begin
+  currentAngle := currentAngle + angle;
+end;
+
+procedure fforward(scale: Integer);
+var
+newX, newY: Integer;
+begin
+  newX := currentX + Round(scale * Cos(currentAngle * Pi / 180));
+  newY := currentY - Round(scale * Sin(currentAngle * Pi / 180));
+  Line(currentX, currentY, newX, newY);
+  currentX := newX;
+  currentY := newY;
+end;
+
+procedure draw(n: Real; ang, scale: Integer);
+begin
+if n > 0 then
+  begin
+    n := n - 0.5;
+    turnRight(ang);
+    draw(n, -ang, scale);
+    fforward(scale);
+    draw(n, ang, scale);
+    fforward(scale);
+    draw(n, -ang, scale);
+    turnLeft(ang);
+  end;
+end;
+
+procedure init(n: Real; x, y, scale: Integer);
+begin
+  currentX := x;
+  currentY := y;
+  currentAngle := 90;
+  draw(n, 90, scale);
+end;
+end.
+
+
+
+
